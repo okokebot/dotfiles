57cf3564a5e06fea83b9d58953716a8754adac59
kokeiro <kokeiro@kokeiro-deps.local>
2019-03-17 15:57:17 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitignore" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
+#    [ "$f" = ".require_oh-my-zsh" ] && continue
     [ "$f" = ".gitmodules" ] && continue
 
     ln -snfv ~/dotfiles/"$f" ~/
@@ -23,12 +23,12 @@ done
 [ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
 
 # emacs set up
-if which cask >/dev/null 2>&1; then
-  echo "setup .emacs.d..."
-  cd ${THIS_DIR}/.emacs.d
-  cask upgrade
-  cask install
-fi
+# if which cask >/dev/null 2>&1; then
+#   echo "setup .emacs.d..."
+#   cd ${THIS_DIR}/.emacs.d
+#   cask upgrade
+#   cask install
+# fi
 
 cat << END
 
